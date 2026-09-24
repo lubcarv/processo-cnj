b49296d57cbe60e8d14d4a51d43aad46afe370e1
lubcarv <luiza.bc26@aluno.ifsc.edu.br>
2026-09-24 10:27:34 -0300

    Identifica o segmento e monta alias do DataJud

--- processo.py
+++ processo.py
@@ -1,3 +1,9 @@
+JUDICIARY_BRANCH_BY_CODE = {"1": "Supremo Tribunal Federal", "2": "Conselho Nacional de Justiça",
+            "3": "Superior Tribunal de Justiça", "4": "Justiça Federal",
+            "5": "Justiça do Trabalho", "6": "Justiça Eleitoral",
+            "7": "Justiça Militar da União", "8": "Justiça dos Estados e do Distrito Federal e Territórios",
+            "9": "Justiça Militar Estadual"}
+
 def split_case_number(case_number):
     clean = case_number.replace(".", "").replace("-", "")
     if clean.isdigit() and len(clean) == 20:
@@ -9,11 +15,29 @@ def split_case_number(case_number):
                 "origin_unit": clean[16:]}
     return None
 
+def describe_case(parts):
+    branch = JUDICIARY_BRANCH_BY_CODE.get(parts["judiciary_branch"])
+    return branch
+
+def build_datajud_alias(parts):
+    branch = parts["judiciary_branch"]
+    if parts["court_code"] == "00" and branch == "5":
+        return "api_publica_tst"
+
+    number = parts["court_code"].lstrip("0")
+    if branch == "4":
+        return  f"api_publica_trf{number}"
+    elif branch == "5":
+        return f"api_publica_trt{number}"
+    return None
 
 case_number = input("Número do processo: ").strip()
 parts = split_case_number(case_number)
 
 if parts:
     print(parts)
+    print(describe_case(parts))
+    print(build_datajud_alias(parts))
 else:
     print("Número de processo inválido")
+
